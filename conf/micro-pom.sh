900c4ad22abca4c7855219bd308f722a5e0b197a
TangBiJing <787740644@qq.com>
2019-05-05 09:17:53 +0800

    test:

--- conf/micro-pom.sh
+++ conf/micro-pom.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
 cat > micro-pom.xml <<EOF
+
 <?xml version="1.0" encoding="UTF-8"?>
 <project xmlns="http://maven.apache.org/POM/4.0.0"
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -20,4 +20,5 @@ cat > micro-pom.xml <<EOF
 
 
 </project>
+
 EOF
