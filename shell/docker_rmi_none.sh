879c06ade062e7e9cd23f523d45da007ccf4b8d7
TangBiJing <787740644@qq.com>
2019-05-08 16:46:39 +0800

    rmi none:

--- shell/docker_rmi_none.sh
+++ shell/docker_rmi_none.sh
@@ -1,3 +1,8 @@
-!/bin/bash
 ## 删除带有<none>的镜像
-docker images|grep none|awk '{print $3}'|xargs docker rmi
+id=`docker images|grep eureka|awk '{print $3}'`
+if [ "$id" != "" ]; then
+  echo "删除<none>镜像"
+  docker rmi $id
+else
+  echo "不存在none镜像"
+fi
